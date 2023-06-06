07d4f8d9fc5e47dbc3f41a7d0fea30040195c1eb
devsdp <devendra.pandey.mit@gmail.com>
2023-06-06 09:54:44 +0530

    environment created

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -2,7 +2,46 @@ from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 from models import (supplier_pydantic,supplier_pydanticIn,Supplier,Products,product_pydantic,product_pydanticIn)
 
+##email
+
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
+from pydantic import EmailStr, BaseModel
+from typing import List
+
+
+#dotenv
+from dotenv import dotenv_values
+
+#credentials
+credentials = dotenv_values(".env")
+
+class EmailSchema(BaseModel):
+    email: List[EmailStr]
+
+class EmailContent(BaseModel):
+    message: str
+    subject : str
+
+
+conf = ConnectionConfig(
+    MAIL_USERNAME = credentials['EMAIL'],
+    MAIL_PASSWORD = credentials['PASS'],
+    MAIL_FROM = "credentials['EMAIL']",
+    MAIL_PORT = 587,
+    MAIL_SERVER = "smtp.gmail.com",
+    # MAIL_STARTTLS = True,
+    MAIL_TLS = True
+    MAIL_SSL = False,
+    USE_CREDENTIALS = True,
+    VALIDATE_CERTS = True
+)
+
+
+
 app = FastAPI()
+
 
 
 @app.get('/')
@@ -45,6 +84,26 @@ async def update_supplier(supplier_id:int, update_info:supplier_pydanticIn):
 async def delete_supplier(supplier_id:int):
     await Supplier.filter(id=supplier_id).delete()
     return{"status":"ok"}
+
+
+
+@app.post("email/{product_id}"):
+async def send_email(product_id:int, content:EmailContent):
+
+    html = f"""<p>Hi this test mail, thanks for using Fastapi-mail</p> """
+
+    @app.post("/email")
+    async def simple_send(email: EmailSchema) -> JSONResponse:
+        message = MessageSchema(
+            subject="Fastapi-Mail module",
+            recipients=email.dict().get("email"),
+            body=html,
+            subtype=MessageType.html)
+
+        fm = FastMail(conf)
+        await fm.send_message(message)
+        return JSONResponse(status_code=200, content={"message": "email has been sent"})
+
 
 
 
